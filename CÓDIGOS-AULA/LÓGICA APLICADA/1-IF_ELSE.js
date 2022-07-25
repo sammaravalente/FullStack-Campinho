60283b992613359926dedd74c1eabe9f796aa94c
Sammara Valente <sammaravs@outlook.com>
2022-07-24 22:20:06 -0300

    Removi pastas upadas de código errado

--- CÓDIGOS-AULA/LÓGICA APLICADA/1-IF_ELSE.js
+++
@@ -1,9 +0,0 @@
-let vingador = "Capitão América";
-
-if(vingador === "Homem de Ferro"){
-    console.log("Sou o melhor vingador")
-}else if(vingador === "Capitão América"){
-    console.log("Cadê o meu escudo?")
-}else{
-    console.log("Hulk esmaga tudo!!!")
-}
